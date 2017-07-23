3dcc7a1b36ddbaa4f964470a3d387bc11d4b2175
theartofnowt <michaelgordon.brown1@gmail.com>
2017-07-23 22:18:33 +0100

    player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,15 @@
+require 'player'
+
+describe Player do
+
+subject(:player) {described_class.new(name)}
+let(:name) { 'Michael' }
+
+it 'returns name' do
+  expect(player.name).to eq name
+end
+
+
+
+
+end
